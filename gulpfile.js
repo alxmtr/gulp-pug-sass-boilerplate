306ab11c01b20380272fe576bd67a90efc144f59
Alexander Ivanov <alexanderivanovset@gmail.com>
2020-02-27 15:02:30 +0300

    my update

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,101 @@
-const { src, dest, watch, series } = require('gulp')
-
-const pug = require('gulp-pug')
-const sass = require('gulp-sass')
-
-const browserSync = require('browser-sync').create()
-
+const { src, dest, watch, series } = require("gulp");
+const pug = require("gulp-pug");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+const babel = require("gulp-babel");
+const babelMinify = require("gulp-babel-minify");
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
+const gulpif = require("gulp-if");
+const cleanCSS = require("gulp-clean-css");
+const include = require("gulp-include");
+const imagemin = require("gulp-imagemin");
+let build = false;
 
 // Compile pug files into HTML
 function html() {
-  return src('src/pug/*.pug')
+  return src("src/pug/*.pug")
     .pipe(pug())
-    .pipe(dest('dist'))
+    .pipe(dest("dist"));
 }
+
+// Compile Js
+function js() {
+  return src("src/js/main.js")
+    .pipe(include())
+    .pipe(sourcemaps.init())
+    .pipe(
+      babel({
+        presets: ["@babel/env"]
+      })
+    )
+    .pipe(gulpif(build, babelMinify()))
+    .pipe(sourcemaps.write())
+    .pipe(dest("dist/js"))
+    .pipe(browserSync.stream());
+}
+
+const sassConfig = {
+  includePaths: ["src/scss"],
+  errLogToConsole: true,
+  outputStyle: "nested",
+  onError: browserSync.notify
+};
 
 // Compile sass files into CSS
 function styles() {
-  return src('src/sass/main.sass')
-    .pipe(sass({
-      includePaths: ['src/sass'],
-      errLogToConsole: true,
-      outputStyle: 'compressed',
-      onError: browserSync.notify
-    }))
-    .pipe(dest('dist/css'))
-    .pipe(browserSync.stream())
+  return src("src/scss/main.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassConfig))
+    .pipe(autoprefixer())
+    .pipe(gulpif(build, cleanCSS()))
+    .pipe(sourcemaps.write())
+    .pipe(dest("dist/css"))
+    .pipe(browserSync.stream());
 }
 
-// Copy assets
-function assets() {
-  return src('src/assets/**/*')
-    .pipe(dest('dist/'))
+// Copy fonts
+function fonts() {
+  return src("src/assets/fonts/*").pipe(dest("dist/assets/fonts/"));
+}
+// copy images or minimise them
+function images() {
+  return src("src/assets/images/**/*")
+    .pipe(gulpif(build, imagemin()))
+    .pipe(dest("dist/images"));
 }
 
-// Serve and watch sass/pug files for changes
+async function turnBuildOn() {
+  build = true;
+}
+
+async function turnBuildOff() {
+  build = false;
+}
+
+// Serve and watch sass/pug/js files for changes
 function watchAndServe() {
   browserSync.init({
-    server: 'dist',
-  })
-
-  watch('src/sass/**/*.sass', styles)
-  watch('src/pug/*.pug', html)
-  watch('src/assets/**/*', assets)
-  watch('dist/*.html').on('change', browserSync.reload)
+    server: "dist"
+  });
+  watch("src/scss/**/*.scss", styles);
+  watch("src/pug/**/*.pug", html);
+  watch("src/js/**/*.js", js);
+  watch("src/assets/fonts/*", fonts);
+  watch("src/assets/images/**/*", images);
+  watch("dist/*.html").on("change", browserSync.reload);
 }
 
-
-exports.html = html
-exports.styles = styles
-exports.watch = watchAndServe
-exports.default = series(html, styles, assets, watchAndServe)
+exports.html = html;
+exports.styles = styles;
+exports.watch = watchAndServe;
+exports.build = series(turnBuildOn, html, styles, js, fonts, images);
+exports.default = series(
+  turnBuildOff,
+  html,
+  styles,
+  js,
+  fonts,
+  images,
+  watchAndServe
+);
---
+++ src/js/partials/header.js
@@ -0,0 +1,5 @@
+console.log("gulp works");
+
+const materials = ["Hydrogen", "Helium", "Lithium", "Beryllium"];
+
+console.log(materials.map(material => material.length));
